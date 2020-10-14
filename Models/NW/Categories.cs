2b55a4d3e8b2f3c88f791da1f82901992fc57c2d
Nate Schmidt <nschmidt1@hotmail.com>
2020-10-13 20:35:35 -0700

    1st commit

---
+++ Models/NW/Categories.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace AspDb.Models.NW
+{
+    public partial class Categories
+    {
+        public Categories()
+        {
+            Products = new HashSet<Products>();
+        }
+
+        public int CategoryId { get; set; }
+        [Required (ErrorMessage = "dont do that")]
+        [Display(Name = "Ayo")]
+        public string CategoryName { get; set; }
+        [Required]
+        public string Description { get; set; }
+        public byte[] Picture { get; set; }
+
+        public virtual ICollection<Products> Products { get; set; }
+    }
+}
